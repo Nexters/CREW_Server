b1e1c6a77b4983cf510ae29711859528e139dd41
kimsejune <tpwns1088@gmail.com>
2019-02-07 15:50:33 +0900

    create form schema

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,7 @@
 import Sequelize from "sequelize";
 import { DbInterface } from "../db/DbInterface/index";
 import { UserFactory } from "./user";
+import { FormFactory } from "./form";
 
 export const createModels = (sequelizeConfig: any): DbInterface => {
   
@@ -9,7 +10,8 @@ export const createModels = (sequelizeConfig: any): DbInterface => {
   const db: DbInterface = {
     sequelize,
     Sequelize,
-    User: UserFactory(sequelize, Sequelize)
+    User: UserFactory(sequelize, Sequelize),
+    Form: FormFactory(sequelize, Sequelize)
   };
   return db;
 }
