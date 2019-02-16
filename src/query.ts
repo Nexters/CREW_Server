aa2aabd8c12ce63b52c5354b20c04f0d1f836673
HarryKim <hangin359@gmail.com>
2019-02-16 17:27:22 +0900

    add query issue #25

--- src/query.ts
+++ src/query.ts
@@ -1,21 +1,20 @@
 import { db } from "./app";
 
 
-export async function findUserById({id}) {
+export async function findUserById({ id }) {
   const user = await db.User.findByPk(id);
-  if(!user) { return null }
+  if (!user) { return null }
   return user;
 };
 
-export async function findUserByProvider({ 
+export async function findUserByProvider({
   member_provider,
   member_provider_number
 }) {
-  const user = await db.User.find({where: {member_provider, member_provider_number}});
-  if(!user) { return null }
+  const user = await db.User.find({ where: { member_provider, member_provider_number } });
+  if (!user) { return null }
   return user;
 }
-
 
 export async function createUser({
   member_provider,
@@ -29,12 +28,27 @@ export async function createUser({
     provide_image,
     token
   });
-  if(!newUser) { return null }
+  if (!newUser) { return null }
   return newUser;
 }
 
-export async function findResumesByUserId({user_id}) {
-  const resumes = await db.Resume.findAll({where: {user_id}});
-  if(!resumes) { return null }
+export async function findResumesByUserId({ user_id }) {
+  const resumes = await db.Resume.findAll({ where: { user_id } });
+  if (!resumes) { return null }
   return resumes;
+}
+
+export async function updateUserInfo({ id, age, name, phone_number, email, job, position }) {
+  const updated_user = await db.User.update(
+    {
+      age: age,
+      name: name,
+      phone_number: phone_number,
+      email: email,
+      job: job,
+      position: position
+    },
+    { where: { id } }
+  );
+  return updated_user;
 }
