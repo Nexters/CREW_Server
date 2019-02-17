08b42a1341056ec9fae2837431e2e041c2272de0
unknown <4whomtbts@gmail.com>
2019-02-18 03:14:14 +0900

    add bracket to util/index

--- src/util/index.ts
+++ src/util/index.ts
@@ -6,17 +6,17 @@ import express from "express";
  *  DEV then LOOSE ERROR
  */
 
- 
-export default class AppResult { 
-    
-    mode  : string;    
-    status : number; 
-    result : any;
-    where :  string;
-    message : string;
+
+export default class AppResult {
+
+    mode: string;
+    status: number;
+    result: any;
+    where: string;
+    message: string;
 
 
-    constructor(result : any = null  , status : number , where : string, message : string){
+    constructor(result: any = null, status: number, where: string, message: string) {
         this.status = status;
         this.result = result;
         this.where = where;
@@ -24,41 +24,42 @@ export default class AppResult {
         this.mode = process.env.MODE;
     }
 
-    public getStatus() : number  {
-        return this.status; 
+    public getStatus(): number {
+        return this.status;
     }
-    public getResult() : void {
-        return this.result; 
+    public getResult(): void {
+        return this.result;
     }
-    public getWhere() : string {
-        return this.where; 
+    public getWhere(): string {
+        return this.where;
     }
-    public getMessage() : string {
-        return this.message; 
+    public getMessage(): string {
+        return this.message;
     }
-    public setResult(result : any) : void {
-        this.result = result; 
+    public setResult(result: any): void {
+        this.result = result;
     }
-    public setMessage(message : string ){
+    public setMessage(message: string) {
         this.message = message;
     }
-    public Error() : string {
-        if(this.mode=="DEV"){
-            let error = "at < "+this.getWhere()+" > : < "+this.getMessage()+" >";
+    public Error(): string {
+        if (this.mode == "DEV") {
+            let error = "at < " + this.getWhere() + " > : < " + this.getMessage() + " >";
             console.log(error);
             return error;
-        }else{
-            return this.getMessage();     
+        } else {
+            return this.getMessage();
         }
     }
-    public Excute(res : express.Response){
-        if(!this.result && this.getStatus() != 200){
+    public Excute(res: express.Response) {
+        if (!this.result && this.getStatus() != 200) {
             res.status(this.getStatus()).send(this.Error())
-        }else if(this.result){
-            res.status(this.getStatus()).end(); 
-        }else{
+        } else if (this.result) {
+            res.status(this.getStatus()).end();
+        } else {
             res.status(504).send("at < util/index > : < server_made_wrong_result > ")
         }
-    
-    
+
+
+    }
 }
