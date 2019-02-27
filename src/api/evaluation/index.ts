7f3715f15608f285f2ce8c8aa3140bd1aa06dcee
kimsejune <tpwns1088@gmail.com>
2019-02-27 11:05:10 +0900

    Add swagger evaluation get Api

--- src/api/evaluation/index.ts
+++ src/api/evaluation/index.ts
@@ -14,7 +14,38 @@ router.use(mw.expressJwt);
 router.use(mw.corsMiddleware);
 router.options('*', mw.corsMiddleware);
 
-
+ /**
+ * @swagger
+ * /evaluations/read?user_id=1:
+ *   get:
+ *     summary: admin이 선택한 user의 평가(evaluations)를 가져온다.
+ *     tags: [Evaluation]
+ *     parameters: 
+ *      - in: user
+ *        name: id
+ *        type: integer
+ *        value: 1
+ *        description: 접속한 id값으로 admin인지 판단한다.
+ *      - in: query
+ *        name: user_id
+ *        type: integer
+ *        value: 2
+ *        description: user_id값으로 admin이 해당 user_id를 가진 사람의 evaluations을 가져온다.
+ *     responses: 
+ *      200: 
+ *       description: admin인지 판단후 query로 받은 user_id에 해당하는 user의 evaluations을 가져온다.
+ *       type: array
+ *       properties: 
+ *        evaluations:
+ *         items: 
+ *          $ref: '#/definitions/Evaluation'
+ *      403: 
+ *       $ref: '#/components/res/Forbidden'
+ *      404: 
+ *       $ref: '#/components/res/NotFound'
+ *      500: 
+ *       $ref: '#/components/res/BadRequest'
+ */
 router.get('/read', async (req: express.Request, res: express.Response) => {
   const user_id = req.query.user_id;
 
